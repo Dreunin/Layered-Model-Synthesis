c2565f0764691fc900c5051d2320463cc29f617e
s204475 <76941623+s204475@users.noreply.github.com>
2025-05-06 17:53:22 +0200

    Log TimeTaken and Possibility in own file

--- Layered Model Synthesis/Assets/Scripts/ModelSynthesis.cs
+++ Layered Model Synthesis/Assets/Scripts/ModelSynthesis.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using NUnit.Framework;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -26,42 +25,7 @@ public class ModelSynthesis : MonoBehaviour
 
     private List<Tile> tiles => tileset.Tiles;
     private Tile border => tileset.Border;
-
-    public class Possibility
-    {
-        public Tile tile;
-        public Rotation rotation;
-        public bool root = true;
-        public bool placed = false;
-
-        public Possibility(Tile tile, Rotation rotation)
-        {
-            this.tile = tile;
-            this.rotation = rotation;
-        }
-
-        public Quaternion GetRotation()
-        {
-            return rotation switch
-            {
-                Rotation.zero => Quaternion.Euler(0, 0, 0),
-                Rotation.ninety => Quaternion.Euler(0, -90, 0),
-                Rotation.oneEighty => Quaternion.Euler(0, -180, 0),
-                Rotation.twoSeventy => Quaternion.Euler(0, -270, 0),
-            };
-        }
-        
-        public override bool Equals(object obj)
-        {
-            if (obj is not Possibility other) return false;
-            return tile == other.tile && rotation == other.rotation;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(tile, (int)rotation);
-        }
-    }
+    private DateTime startTime;
     
     /// <summary>
     /// Checks whether a coordinate is inside the grid area
@@ -88,6 +52,7 @@ public class ModelSynthesis : MonoBehaviour
 
         Random.InitState(seed);
         parentTransform = new GameObject($"Room {seed}").transform;
+        startTime = DateTime.Now;
         Synthesise();
     }
 
@@ -131,7 +96,8 @@ public class ModelSynthesis : MonoBehaviour
             }
         }
         if(animate) StartCoroutine(nameof(AnimatePlaceTiles));
-        Debug.Log("Model Synthesis complete.");
+        double timeTaken = (DateTime.Now - startTime).TotalSeconds;
+        Debug.Log($"Model Synthesis complete. Took {(int)timeTaken} seconds.");
     }
     
     /// <summary>
---
+++ Layered Model Synthesis/Assets/Scripts/Possibility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class Possibility 
+{
+    public Tile tile;
+    public Rotation rotation;
+    public bool root = true;
+    public bool placed = false;
+
+    public Possibility(Tile tile, Rotation rotation)
+    {
+        this.tile = tile;
+        this.rotation = rotation;
+    }
+
+    public Quaternion GetRotation()
+    {
+        return rotation switch
+        {
+            Rotation.zero => Quaternion.Euler(0, 0, 0),
+            Rotation.ninety => Quaternion.Euler(0, -90, 0),
+            Rotation.oneEighty => Quaternion.Euler(0, -180, 0),
+            Rotation.twoSeventy => Quaternion.Euler(0, -270, 0),
+        };
+    }
+    
+    public override bool Equals(object obj)
+    {
+        if (obj is not Possibility other) return false;
+        return tile == other.tile && rotation == other.rotation;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(tile, (int)rotation);
+    }
+}
