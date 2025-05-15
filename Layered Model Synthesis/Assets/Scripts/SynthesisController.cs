30cd101218d02ef93d5fb69798ff6a6d5c71fef1
Benjamin Lumbye <benjamin@lumbye.net>
2025-05-15 17:10:35 +0200

    Add room offset to handle synthesis of multiple rooms at once

--- Layered Model Synthesis/Assets/Scripts/SynthesisController.cs
+++ Layered Model Synthesis/Assets/Scripts/SynthesisController.cs
@@ -154,7 +154,7 @@ public class SynthesisController : MonoBehaviour
         }
         Transform layer = room.GetChild(position.y);
         
-        Vector3 placement = position + new Vector3(possibility.tile.GetRotatedSize(possibility.rotation).x-1,0, possibility.tile.GetRotatedSize(possibility.rotation).z-1) / 2f;
+        Vector3 placement = position + new Vector3(possibility.tile.GetRotatedSize(possibility.rotation).x-1,0, possibility.tile.GetRotatedSize(possibility.rotation).z-1) / 2f + room.position;
         Tile newTile = Instantiate(possibility.tile, placement, possibility.GetQuaternion());
         newTile.transform.SetParent(layer);
         if (possibility.tile.allowFreeRotation) newTile.transform.eulerAngles = new Vector3(0, Random.Range(0,360), 0);
